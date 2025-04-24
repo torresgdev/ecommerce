71fad003ce81adab5d2c77075dbf6194b173910e
Gabriel Torres <gbrtorress@gmail.com>
2025-04-24 08:44:18 -0300

    added user.service

---
+++ src/users/service/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "../user.entity";
+import { CreateUserDto } from "../dto/create-user.dto";
+import { UpdateUserDto } from "../dto/update-user.dto";
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private repo: Repository<User>
+    ) {}
+
+    create(dto: CreateUserDto) {
+        const user = this.repo.create(dto);
+        return this.repo.save(user);
+    }
+
+    findAll() {
+        return this.repo.find()
+    }
+
+    findById(id: number) {
+        return this.repo.findOneBy({id})
+    }
+
+    update(id: number, dto: UpdateUserDto) {
+        return this.repo.update(id, dto)
+    }
+
+    remove(id:number) {
+        return this.repo.delete(id);
+    }
+}
