ffebf961d0d22c523a55f69b17c7169e9f28bc9a
Gabriel Torres <gbrtorress@gmail.com>
2025-04-24 09:09:19 -0300

    added swagger for test, update some files for swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('API para um sistema de pedidos (e-commerce básico)')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // http://localhost:3000/api
+
+  await app.listen(3000);
 }
 bootstrap();
--- src/users/controller/users.controller.ts
+++ src/users/controller/users.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Body, Post, Get, Put, Delete, Param } from "@nestjs/common"
 import { UserService } from "../service/users.service";
 import { CreateUserDto } from "../dto/create-user.dto";
 import { UpdateUserDto } from "../dto/update-user.dto";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @ApiTags('Users')
 @Controller('users')
@@ -10,26 +10,32 @@ export class UserController {
     constructor(private readonly service: UserService) {}
 
     @Post()
+    @ApiOperation({ summary: 'Cria um novo usuário' })
+    @ApiResponse({ status: 201, description: 'Usuário criado com sucesso.' })
     create(@Body() body: CreateUserDto) {
         return this.service.create(body)
     }
 
     @Get()
+    @ApiOperation({ summary: 'Lista todos os usuários' })
     findAll() {
         return this.service.findAll()
     }
 
     @Get(':id')
+    @ApiOperation({ summary: 'Busca usuário por ID' })
     findOne(@Param('id') id:string) {
         return this.service.findById
     }
     
     @Put(':id')
+    @ApiOperation({ summary: 'Atualiza um usuário' })
     update(@Param('id') id:string, @Body() body: UpdateUserDto) {
         return this.service.update(+id, body)
     }
 
     @Delete(':id')
+    @ApiOperation({ summary: 'Remove um usuário' })
     remove(@Param('id') id:string) {
         return this.service.remove(+id)
     }
